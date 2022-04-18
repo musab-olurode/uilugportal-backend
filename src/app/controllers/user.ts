c7e2fd7d78ad8d9fbfc79d73abec1b756a41f844
musab-olurode <olurodemusab@gmail.com>
2022-04-18 17:32:06 +0100

    chore: remove unused controller arguments

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -1,33 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { SuccessResponse } from '../../core/ApiResponse';
 import { IIdTokens } from '../../interfaces/IdTokens';
 import { getSessionsAsString } from '../helpers/constants';
 import asyncHandler from '../middlewares/async';
 import UserService from '../services/user';
 
-// eslint-disable-next-line no-unused-vars
-export const getResult = asyncHandler(
-	// eslint-disable-next-line no-unused-vars
-	async (req: Request, res: Response, next: NextFunction) => {
-		const sessionsAsString = getSessionsAsString();
-		await req.validate({
-			session: 'required|string|in:' + sessionsAsString,
-		});
+export const getResult = asyncHandler(async (req: Request, res: Response) => {
+	const sessionsAsString = getSessionsAsString();
+	await req.validate({
+		session: 'required|string|in:' + sessionsAsString,
+	});
 
-		const results = await UserService.getResults(
-			req.sessionId as string,
-			req.query.session as string
-		);
+	const results = await UserService.getResults(
+		req.sessionId as string,
+		req.query.session as string
+	);
 
-		return new SuccessResponse('results retrieved successfully', {
-			results,
-		}).send(res);
-	}
-);
+	return new SuccessResponse('results retrieved successfully', {
+		results,
+	}).send(res);
+});
 
 export const calculateCGPA = asyncHandler(
-	// eslint-disable-next-line no-unused-vars
-	async (req: Request, res: Response, next: NextFunction) => {
+	async (req: Request, res: Response) => {
 		await req.validate({
 			level: 'required|string|in:100,200,300,400,500',
 		});
@@ -44,8 +39,7 @@ export const calculateCGPA = asyncHandler(
 );
 
 export const getAllPrintables = asyncHandler(
-	// eslint-disable-next-line no-unused-vars
-	async (req: Request, res: Response, next: NextFunction) => {
+	async (req: Request, res: Response) => {
 		const sessionsAsString = getSessionsAsString();
 		await req.validate({
 			session: 'required|string|in:' + sessionsAsString,
